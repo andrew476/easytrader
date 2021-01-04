4bfdcdb1cd9b138b9074fb7f7bb161768299c922
Zhang Luoxian <59600044+andrew476@users.noreply.github.com>
2021-01-04 17:28:04 +0800

    没看完呢

--- easytrader/api.py
+++ easytrader/api.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 import logging
+#导入日志库，抛弃print
 import sys
+#sys是针对与Python解释器相关的变量和方法
 
 import six
-
+#six库是对于python2和python3的兼容库，使用six库可以使写的代码不用进行修改就能在python2或者python3的环境下进行运行
 from easytrader.joinquant_follower import JoinQuantFollower
+#从易贸.joinquant_follower 中导入JoinQuantFollower类
 from easytrader.log import logger
+#从易贸.log中导入logger变量
 from easytrader.ricequant_follower import RiceQuantFollower
+
 from easytrader.xq_follower import XueQiuFollower
 from easytrader.xqtrader import XueQiuTrader
-
+#同上，导入功能函数
 if sys.version_info <= (3, 5):
     raise TypeError("不支持 Python3.5 及以下版本，请升级")
-
-
+#获取元组类型的版本信息
+#下面定义了一些功能函数
 def use(broker, debug=False, **kwargs):
     """用于生成特定的券商对象
     :param broker:券商名支持 ['yh_client', '银河客户端'] ['ht_client', '华泰客户端']
@@ -29,10 +34,10 @@ def use(broker, debug=False, **kwargs):
     """
     if debug:
         logger.setLevel(logging.DEBUG)
-
+#如果debug为真，则logger = logging.getLogger("easytrader")=返回指定名称的记录器。设置此记录器的日志记录级别。级别必须是int或str。
     if broker.lower() in ["xq", "雪球"]:
         return XueQiuTrader(**kwargs)
-
+#broker中介。
     if broker.lower() in ["yh_client", "银河客户端"]:
         from .yh_clienttrader import YHClientTrader
 
